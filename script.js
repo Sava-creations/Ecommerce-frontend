c1c8e8886ec065a1b5a90529021d02e9783860b9
Savandi <savandikodithuwakku@gmail.com>
2025-02-21 02:14:12 +0530

    Navigation has added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-var MainImg=document.getElementById("MainImg");
-var smallImg=document.getElementsByClassName("small-img");  
+var MainImg=document.getElementById("MainImg");                               
+var smallImg=document.getElementsByClassName("small-img");                      
 
-smallImg[0].onclick=function(){
-    MainImg.src=smallImg[0].src;
+smallImg[0].onclick=function(){                                                  //smallImg[0] this will refer to the first small image it its counted with index 
+    MainImg.src=smallImg[0].src;                                                 //MainImg s image file path is replaced with small image s  file path
 }
 smallImg[1].onclick=function(){
     MainImg.src=smallImg[1].src;
@@ -13,3 +13,9 @@ smallImg[2].onclick=function(){
 smallImg[3].onclick=function(){
     MainImg.src=smallImg[3].src;
 }
+
+// for (let i = 0; i < smallImg.length; i++) {
+//     smallImg[i].onclick = function() {
+//         MainImg.src = this.src;                                                 // "this" refers to the clicked small image
+//     };
+// }
